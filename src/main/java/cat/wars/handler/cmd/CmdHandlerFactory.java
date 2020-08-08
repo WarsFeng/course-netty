b48cea9070be5213abd1f80bef712a96f24cd7d3
wars <huangrongfeng1999@gmail.com>
2020-08-08 15:55:47 +0800

    refactor(handler): Cmd handler factory optimized

--- src/main/java/cat/wars/handler/cmd/CmdHandlerFactory.java
+++ src/main/java/cat/wars/handler/cmd/CmdHandlerFactory.java
@@ -5,6 +5,8 @@ import cat.wars.util.PackageUtil;
 import com.google.protobuf.GeneratedMessageV3;
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -26,16 +28,26 @@ public final class CmdHandlerFactory {
   public static void init() {
     Class<?>[] cmdClasses = MessageProtocol.class.getDeclaredClasses();
     Set<Class<?>> handlerClasses = PackageUtil.listSubClazz("cat.wars.handler.cmd", false, CmdHandler.class);
-    // Make tmp handler Map<SimpleName, Class>
-    Map<String, Class<?>> tmpHandlerMap = new HashMap<>();
+    // Make tmp handler Map<CmdClass, HandlerClass>
+    Map<Class<?>, Class<?>> tmpHandlerMap = new HashMap<>();
     for (Class<?> handlerClass : handlerClasses) {
-      tmpHandlerMap.put(handlerClass.getSimpleName().toLowerCase().replaceAll("handler", ""), handlerClass);
+      if (Modifier.isAbstract(handlerClass.getModifiers())) continue;
+      Method[] methods = handlerClass.getDeclaredMethods();
+
+      for (Method method : methods) {
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        if (!"handle".equals(method.getName())) continue;
+        if (2 > parameterTypes.length
+                || !GeneratedMessageV3.class.isAssignableFrom(parameterTypes[1]))
+          continue;
+
+        tmpHandlerMap.put(parameterTypes[1], handlerClass);
+      }
     }
 
     for (Class<?> cmdClass : cmdClasses) {
       if (!GeneratedMessageV3.class.isAssignableFrom(cmdClass)) continue;
-      String slimName = cmdClass.getSimpleName().toLowerCase();
-      Class<?> handlerClass = tmpHandlerMap.get(slimName); // Try get handler
+      Class<?> handlerClass = tmpHandlerMap.get(cmdClass); // Try get handler
       if (null == handlerClass) continue;
       log.info("Scanned handler: {} <-> {}", cmdClass.getName(), handlerClass.getName());
 
