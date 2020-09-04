99cfa9d8cda04c10ff2aac2047721a82bcb83dac
wars <wars@wars.cat>
2020-09-04 11:45:09 +0800

    feat(handler): User attk add target user die

--- src/main/java/cat/wars/handler/cmd/UserAttkCmdHandler.java
+++ src/main/java/cat/wars/handler/cmd/UserAttkCmdHandler.java
@@ -2,6 +2,8 @@ package cat.wars.handler.cmd;
 
 import cat.wars.handler.Broadcaster;
 import cat.wars.model.MessageProtocol;
+import cat.wars.model.user.User;
+import cat.wars.model.user.UserManager;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.util.AttributeKey;
 
@@ -17,15 +19,26 @@ public class UserAttkCmdHandler implements CmdHandler<MessageProtocol.UserAttkCm
   public void handle(ChannelHandlerContext context, MessageProtocol.UserAttkCmd cmd) {
     if (null == context || null == cmd) return;
     Integer userId = (Integer) context.channel().attr(AttributeKey.valueOf("userId")).get();
-    if (null == userId) return;
+    if (null == userId || 0 >= cmd.getTargetUserId()) return;
+    User targetUser = UserManager.getUserById(cmd.getTargetUserId());
 
-    MessageProtocol.UserAttkResult userAttkResult = MessageProtocol.UserAttkResult.newBuilder()
+    if (0 >= targetUser.getCurrHP().addAndGet(-10)) {
+      MessageProtocol.UserDieResult userDieResult =
+          MessageProtocol.UserDieResult.newBuilder()
+              .setTargetUserId(targetUser.getUserId())
+              .build();
+      Broadcaster.broadcast(userDieResult);
+    }
+
+    MessageProtocol.UserAttkResult userAttkResult =
+        MessageProtocol.UserAttkResult.newBuilder()
             .setAttkUserId(userId)
             .setTargetUserId(cmd.getTargetUserId())
             .build();
     Broadcaster.broadcast(userAttkResult);
 
-    MessageProtocol.UserSubtractHpResult userSubtractHpResult = MessageProtocol.UserSubtractHpResult.newBuilder()
+    MessageProtocol.UserSubtractHpResult userSubtractHpResult =
+        MessageProtocol.UserSubtractHpResult.newBuilder()
             .setTargetUserId(cmd.getTargetUserId())
             .setSubtractHp(10)
             .build();
--- src/main/java/cat/wars/model/user/User.java
+++ src/main/java/cat/wars/model/user/User.java
@@ -3,6 +3,8 @@ package cat.wars.model.user;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @program: netty-demo
  * @description: User model
@@ -16,20 +18,22 @@ public class User {
   public User(Integer userId, String heroAvatar) {
     this.userId = userId;
     this.heroAvatar = heroAvatar;
+    this.totalHP = new AtomicInteger(100);
+    this.currHP = new AtomicInteger(100);
   }
 
-  /**
-   * 用户 Id
-   */
+  /** 用户 Id */
   private Integer userId;
 
-  /**
-   * 英雄形象
-   */
+  /** 英雄形象 */
   private String heroAvatar;
 
-  /**
-   * 用户移动状态
-   */
+  /** 用户移动状态 */
   private UserMoveState userMoveState;
+
+  /** 用户当前血量 */
+  private AtomicInteger currHP;
+
+  /** 用户总血量 */
+  private AtomicInteger totalHP;
 }
