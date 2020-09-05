c46aeb77bc109a88651ec925f6962b2e386505bb
wars <wars@wars.cat>
2020-09-05 11:24:39 +0800

    refactor(handler,thread): Changed from netty thread management to single thread management
    * Added MainThreadProcessor.java

--- src/main/java/cat/wars/handler/MessageHandler.java
+++ src/main/java/cat/wars/handler/MessageHandler.java
@@ -1,10 +1,8 @@
 package cat.wars.handler;
 
-import cat.wars.handler.cmd.CmdHandler;
-import cat.wars.handler.cmd.CmdHandlerFactory;
 import cat.wars.model.MessageProtocol;
 import cat.wars.model.user.UserManager;
-import com.google.protobuf.GeneratedMessageV3;
+import cat.wars.thread.MainThreadProcessor;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.AttributeKey;
@@ -34,26 +32,13 @@ public class MessageHandler extends SimpleChannelInboundHandler<Object> {
     Broadcaster.removeChannel(context.channel());
     UserManager.removeUser(userId);
 
-    MessageProtocol.UserQuitResult.Builder resultBuilder = MessageProtocol.UserQuitResult.newBuilder()
-            .setQuitUserId(userId);
+    MessageProtocol.UserQuitResult.Builder resultBuilder =
+        MessageProtocol.UserQuitResult.newBuilder().setQuitUserId(userId);
     Broadcaster.broadcast(resultBuilder.build());
   }
 
   @Override
   protected void channelRead0(ChannelHandlerContext context, Object msg) {
-    if (null == msg) return;
-
-    CmdHandler<? extends GeneratedMessageV3> cmdHandler = CmdHandlerFactory.create(msg.getClass());
-    if (null == cmdHandler) {
-      log.error("Does Not match the handler!");
-      return;
-    }
-
-    cmdHandler.handle(context, cast(msg));
-  }
-
-  private static <Cmd extends GeneratedMessageV3> Cmd cast(Object msg) {
-    if (null == msg) return null;
-    return (Cmd) msg;
+    MainThreadProcessor.getInstance().handler(context, msg);
   }
 }
---
+++ src/main/java/cat/wars/thread/MainThreadProcessor.java
@@ -0,0 +1,43 @@
+package cat.wars.thread;
+
+import cat.wars.handler.cmd.CmdHandler;
+import cat.wars.handler.cmd.CmdHandlerFactory;
+import com.google.protobuf.GeneratedMessageV3;
+import io.netty.channel.ChannelHandlerContext;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public final class MainThreadProcessor {
+
+  @Getter private static final MainThreadProcessor instance = new MainThreadProcessor();
+  private final ExecutorService executorService =
+      Executors.newSingleThreadExecutor(r -> new Thread(r, "MainThreadProcessor"));
+
+  private MainThreadProcessor() {}
+
+  public void handler(ChannelHandlerContext context, Object msg) {
+    executorService.submit(
+        () -> {
+          if (null == context || null == msg) return;
+
+          CmdHandler<? extends GeneratedMessageV3> cmdHandler =
+              CmdHandlerFactory.create(msg.getClass());
+          if (null == cmdHandler) {
+            log.error("Does Not match the handler!");
+            return;
+          }
+
+          log.info(Thread.currentThread().getName());
+          cmdHandler.handle(context, cast(msg));
+        });
+  }
+
+  private static <Cmd extends GeneratedMessageV3> Cmd cast(Object msg) {
+    if (null == msg) return null;
+    return (Cmd) msg;
+  }
+}
